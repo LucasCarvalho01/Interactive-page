bf7d71af9a3ada81e828cc9bbbec3c34037fb83b
Lucas Carvalho <lucascarvalhopc@gmail.com>
2022-07-06 11:21:07 -0300

    completed js for menu mobile

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -4,22 +4,38 @@ export default class Menu {
     this.actualMenu = document.querySelector('.menu');
 
     this.openMenu = this.openMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
   }
 
   openMenu() {
-    this.actualMenu.style.display = 'flex';
-    this.changeIconMenu();
-    window.addEventListener('touchend', () => {
-      console.log(event);
-    })
+    this.actualMenu.classList.toggle('active');
+    this.eventClose();
+  }
+
+  // When menu is opened, add eventlistener on window for touch.
+  // If touch was not inside the menu, close it and remove the eventlistener
+  closeMenu(event) {
+    console.log(event.target);
+    if(event.target.tagName !== 'A' && event.target !== this.menu.firstElementChild) {
+      this.actualMenu.classList.remove('active');
+      window.removeEventListener('touchstart', this.closeMenu);
+    }
   }
 
   addEventMenu() {
-    this.menu.addEventListener('touchend', this.openMenu);
+    this.menu.addEventListener('touchstart', this.openMenu);
+  }
+
+  eventClose() {
+    window.addEventListener('touchstart', this.closeMenu);
   }
 
   changeIconMenu() {
     this.menu.children[0].setAttribute('src', './images/icon-close-menu.svg');
+  }
+
+  changeIconHam() {
+    this.menu.children[0].setAttribute('src', './images/icon-hamburger.svg');
   }
 
   init() {
